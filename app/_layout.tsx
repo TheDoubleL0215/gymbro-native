dff67b725d3039bd120c480bdcf0db100ea6229c
LevaiLevente <levai.levente.gabor@gmail.com>
2025-01-24 13:02:15 +0100

    db prework

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,25 +1,35 @@
 import { Stack } from "expo-router";
 import "../global.css";
-import { StatusBar, View } from "react-native";
+import { ActivityIndicator, StatusBar, View } from "react-native";
 import { ThemeProvider } from "@react-navigation/native";
 import { AppTheme } from "@/styles/AppTheme";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import React from "react";
+import React, { Suspense } from "react";
+import { SQLiteProvider } from "expo-sqlite";
+
+export const DATABASE_NAME = "exercise";
 
 export default function RootLayout() {
   return (
     <>
-      <StatusBar barStyle="light-content" backgroundColor="black" />
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <ThemeProvider value={AppTheme}>
-          <View style={{ flex: 1, backgroundColor: AppTheme.colors.background }}>
-            <Stack>
-              {/* Ensure `(tabs)` exists and is correctly set up */}
-              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            </Stack>
-          </View>
-        </ThemeProvider>
-      </GestureHandlerRootView>
+      <Suspense fallback={<ActivityIndicator size="large" />}>
+        <SQLiteProvider
+          databaseName={DATABASE_NAME}
+          options={{ enableChangeListener: true }}
+          useSuspense>
+          <StatusBar barStyle="light-content" backgroundColor="black" />
+          <GestureHandlerRootView style={{ flex: 1 }}>
+            <ThemeProvider value={AppTheme}>
+              <View style={{ flex: 1, backgroundColor: AppTheme.colors.background }}>
+                <Stack>
+                  {/* Ensure `(tabs)` exists and is correctly set up */}
+                  <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                </Stack>
+              </View>
+            </ThemeProvider>
+          </GestureHandlerRootView>
+        </SQLiteProvider>
+      </Suspense>
     </>
   );
 }
